1fa79d83c20ffafe40aee6b094a055a5ed4d5e8e
Adriano Parente <adriano.parente@gmx.de>
2021-08-19 19:32:43 +0200

    user array

--- js/board.js
+++ js/board.js
@@ -23,42 +23,31 @@ function loadAllTasks() {
 }
 
 function updateHTML() {
-
     let todo = allTasksOptimzed.filter(t => t['status'] == 'todo');
-
+    let inProgress = allTasksOptimzed.filter(t => t['status'] == 'inProgress');
+    let testing = allTasksOptimzed.filter(t => t['status'] == 'testing');
+    let done = allTasksOptimzed.filter(t => t['status'] == 'done');
     document.getElementById('todo').innerHTML = '';
-
+    document.getElementById('inProgress').innerHTML = '';
+    document.getElementById('testing').innerHTML = '';
+    document.getElementById('done').innerHTML = '';
     for (let i = 0; i < todo.length; i++) {
         const element = todo[i];
         document.getElementById('todo').innerHTML += generateToDoElement(element);
     }
-
-    let inProgress = allTasksOptimzed.filter(t => t['status'] == 'inProgress');
-
-    document.getElementById('inProgress').innerHTML = '';
-
     for (let i = 0; i < inProgress.length; i++) {
         const element = inProgress[i];
         document.getElementById('inProgress').innerHTML += generateToDoElement(element);
     }
-
-    let testing = allTasksOptimzed.filter(t => t['status'] == 'testing');
-
-    document.getElementById('testing').innerHTML = '';
-
     for (let i = 0; i < testing.length; i++) {
         const element = testing[i];
         document.getElementById('testing').innerHTML += generateToDoElement(element);
     }
-
-    let done = allTasksOptimzed.filter(t => t['status'] == 'done');
-
-    document.getElementById('done').innerHTML = '';
-
     for (let i = 0; i < done.length; i++) {
         const element = done[i];
         document.getElementById('done').innerHTML += generateToDoElement(element);
     }
+      
 }
 
 function startDragging(id) {
